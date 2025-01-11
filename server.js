5ef3ca5ae87b7a29b56d7a0318e5c443571de4c3
edupoly <info.edupoly@gmail.com>
2025-01-11 18:54:18 +0530

    addplanbased

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const ObjectId = mongoose.Types.ObjectId;
 var cors = require('cors');
 var User = require('./models/user');
 var Loan = require('./models/loan');
+var Businessaccount = require('./models/businessAccount')
+var Customer = require("./models/customer")
 var app = express();
 
 app.use(cors());
@@ -138,6 +140,109 @@ app.patch('/updateAllEmis/:id', (req, res) => {
       res.status(500).send("Error updating installments");
     });
   });
+
+  /* planbased billing endpoints */
+
+app.post('/addBusinessAccount', (req, res) => {
+    let newBusinessAccount = new Businessaccount(req.body);
+    newBusinessAccount.save().then((bacc) => {
+        res.json(bacc.businessName);
+    });
+});
+
+app.post('/getBusiness', (req, res) => {
+    Businessaccount.find({ primaryMobile: req.body.primaryMobile, password: req.body.password }).then((business) => {
+        res.json(business);
+    })
+})
+
+app.patch('/updatePlansById/:id', (req, res) => {
+    Businessaccount.findByIdAndUpdate(
+        { _id: req.params.id },
+        { $push: { plans: { $each: req.body.plans } } },
+        { new: true }
+    ).then((business) => {
+        res.json(business);
+    })
+})
+
+app.patch('/updateServicesById/:id', (req, res) => {
+    Businessaccount.findByIdAndUpdate(
+        { _id: req.params.id },
+        { $push: { services: { $each: req.body.services } } },
+        { new: true }
+    ).then((business) => {
+        res.json(business);
+    })
+})
+
+app.post('/addCustomer/:id', (req, res) => {
+    Businessaccount.findByIdAndUpdate(
+        { _id: req.params.id },
+        { $push: { customers: req.body } },
+        { new: true }
+    ).then((business) => {
+        let newCustomer = new Customer({ mobile: req.body.mobile });
+        newCustomer.save().then((cust) => {
+            res.json(req.body.mobile);
+        });
+    })
+})
+
+app.post('/getCustomer', (req, res) => {
+    Customer.find({ mobile: req.body.mobile }).then((customer) => {
+        res.json(customer);
+    })
+})
+
+app.patch('/createPassword/:mobile', (req, res) => {
+    Customer.findOneAndUpdate(
+        { mobile: req.params.mobile },
+        { $set: { password: req.body.password } },
+        { new: true }
+    ).then((customer) => {
+        res.json(customer);
+    })
+})
+
+app.get('/getCustomerDetails/:bId/:mobile', (req, res) => {
+    Businessaccount.aggregate([
+        { $match: { _id: new ObjectId(req.params.bId) } },
+        {
+            $project: {
+                customers: {
+                    $filter: {
+                        input: "$customers",
+                        as: "customer",
+                        cond: { $eq: ["$$customer.mobile", req.params.mobile] }
+                    }
+                }
+            }
+        }
+    ]).then((customer) => {
+        res.json(customer);
+    })
+})
+
+app.get('/getServices/:bId',(req,res)=>{
+    Businessaccount.aggregate([
+        { $match: { _id: new ObjectId(req.params.bId) } },
+        { $project : { services: 1 } }
+    ]).then((services)=>{
+        res.json(services);
+    })
+})
+
+app.post('/addTransaction/:bId', (req, res) => {
+    Businessaccount.findByIdAndUpdate(
+        { _id: req.params.bId },
+        { $push: { transactions: req.body } },
+        { new: true }
+    ).then((business) => {
+        res.json(req.body.totalBill);
+    })
+})
+
 
 app.listen(4600, () => {
     console.log('running at 4600')
